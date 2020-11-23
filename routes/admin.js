694f439c4dd607ac7b2d06fa60857274d25304ce
Jason Deng <foralief@gmail.com>
2020-11-22 23:42:26 -0600

    Finished up to 108

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,22 @@
-const path = require("path");
-
 const express = require("express");
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", productsController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
+
+// /admin/edit-product => PUT
+// router.put("/edit-product", adminController.putEditProduct);
+
+// /admin/edit-product => DELETE
+// router.delete("/edit-product", adminController.deleteEditProduct);
 
 module.exports = router;
