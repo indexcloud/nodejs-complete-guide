9d82333197f05eea89e3d1903d34cd03256e12cf
Jason Deng <foralief@gmail.com>
2020-11-29 23:31:33 -0600

    Finished Section 11

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const path = require("path");
 const express = require("express");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -19,9 +25,51 @@ app.use(express.urlencoded({extended: false}));
 // request body has been url encoded
 app.use(express.static(path.join(__dirname, "public"))); // link to css
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then(user => {
+			req.user = user; // This is sequelize object, not JavasScript object
+			next();
+		})
+		.catch(err => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use("*", errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {constraints: true, onDelete: "CASCADE"});
+User.hasMany(Product);
+Cart.belongsTo(User);
+User.hasOne(Cart);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+Product.belongsToMany(Order, {through: OrderItem});
+
+sequelize
+	// .sync({force: true})
+	.sync()
+	.then(result => {
+		return User.findByPk(1);
+		// console.log(result);
+	})
+	.then(user => {
+		if (!user) {
+			return User.create({name: "Max", email: "test@test.com"});
+		}
+		return user;
+	})
+	.then(user => {
+		// console.log(user);
+		return user.createCart();
+	})
+	.then(cart => {
+		app.listen(3000);
+	})
+	.catch(err => {
+		console.log(err);
+	});
