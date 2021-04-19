c1a1704ea367bad63c3a7b28da498816fa16d825
Jason Deng <foralief@gmail.com>
2021-04-18 23:19:34 -0500

    Finished up to Section 15 259

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
@@ -18,14 +20,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById("607ce5744ce11f59ca79eb95")
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({email: email})
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save(err => {
-				console.log(err);
-				res.redirect("/");
-			});
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save(err => {
+							console.log(err);
+							res.redirect("/");
+						});
+					}
+					res.redirect("/login");
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect("/login");
+				});
 		})
 		.catch(err => console.log(err));
 };
@@ -40,15 +58,19 @@ exports.postSignup = (req, res, next) => {
 			if (userDoc) {
 				return res.redirect("/signup");
 			}
-			const user = new User({
-				email: email,
-				password: password,
-				cart: {items: []},
-			});
-			return user.save();
-		})
-		.then(result => {
-			res.redirect("/login");
+			return bcrypt
+				.hash(password, 12)
+				.then(hashedPassword => {
+					const user = new User({
+						email: email,
+						password: hashedPassword,
+						cart: {items: []},
+					});
+					return user.save();
+				})
+				.then(result => {
+					res.redirect("/login");
+				});
 		})
 		.catch(err => console.log(err));
 };
