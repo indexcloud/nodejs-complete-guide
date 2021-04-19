0c12af3c3dd12f96c7c511bb7871bdf0c4a51bf4
Jason Deng <foralief@gmail.com>
2021-04-18 21:26:17 -0500

    Finished up to Section 15 254

--- app.js
+++ app.js
@@ -34,10 +34,23 @@ app.use(
 	session({
 		secret: "my secret",
 		resave: false,
-		saveUniitialized: false,
+		saveUninitialized: false,
 		store: store,
+		// cookie: {maxAge: 6000},
 	})
 );
+
+app.use((req, res, next) => {
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then(user => {
+			req.user = user;
+			next();
+		})
+		.catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -48,18 +61,6 @@ app.use(errorController.get404);
 mongoose
 	.connect(MONGODB_URI)
 	.then(result => {
-		User.findOne().then(user => {
-			if (!user) {
-				const user = new User({
-					name: "Max",
-					email: "max@test.com",
-					cart: {
-						items: [],
-					},
-				});
-				user.save();
-			}
-		});
 		app.listen(3000);
 	})
 	.catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -9,12 +9,53 @@ exports.getLogin = (req, res, next) => {
 	});
 };
 
+exports.getSignup = (req, res, next) => {
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false,
+	});
+};
+
 exports.postLogin = (req, res, next) => {
-	User.findById("607b4af14e6a453773c6727a")
+	User.findById("607ce5744ce11f59ca79eb95")
 		.then(user => {
 			req.session.isLoggedIn = true;
 			req.session.user = user;
-			res.redirect("/");
+			req.session.save(err => {
+				console.log(err);
+				res.redirect("/");
+			});
 		})
 		.catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
+
+	User.findOne({email: email})
+		.then(userDoc => {
+			if (userDoc) {
+				return res.redirect("/signup");
+			}
+			const user = new User({
+				email: email,
+				password: password,
+				cart: {items: []},
+			});
+			return user.save();
+		})
+		.then(result => {
+			res.redirect("/login");
+		})
+		.catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+	req.session.destroy(err => {
+		console.log(err);
+		res.redirect("/");
+	});
 };
