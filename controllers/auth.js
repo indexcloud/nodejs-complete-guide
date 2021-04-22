cd1c07c2fc1251c45a8533647acdf3365726e40c
Jason Deng <foralief@gmail.com>
2021-04-21 23:55:08 -0500

    Finished up to Section 18 294

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require("crypto");
 
 const bcrypt = require("bcryptjs");
 const sgMail = require("@sendgrid/mail");
+const {validationResult} = require("express-validator/check");
 
 const User = require("../models/user");
 
@@ -70,6 +71,16 @@ exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	const confirmPassword = req.body.confirmPassword;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		console.log(errors.array());
+		return res.status(422).render("auth/signup", {
+			// 422 indicate validation fail
+			path: "/signup",
+			pageTitle: "Signup",
+			errorMessage: errors.array()[0].msg,
+		});
+	}
 
 	User.findOne({email: email})
 		.then(userDoc => {
